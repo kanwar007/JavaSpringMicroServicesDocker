bcdab443c27a741d46574130f9eecf515a33b774
kanwar007 <vj.007kkanwar@gmail.com>
2023-06-02 11:47:58 +0800

    project added

---
+++ example/src/test/java/com/vijay/microservices/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijay.microservices.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
